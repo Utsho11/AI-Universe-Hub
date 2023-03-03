b64328949138a8af0557261a697c0a6ebd24a667
Utsho11 <utshoroy521@gmail.com>
2023-03-03 12:28:20 +0600

    all card added

--- js/index.js
+++ js/index.js
@@ -2,11 +2,27 @@ const loadAiTools = async() =>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data = await res.json();
-    displayAiTools(data.data.tools[0]);
+    displayAiTools(data.data.tools);
 }
 
 const displayAiTools = tools =>{
-    console.log(tools);
+    const AiToolContainer = document.getElementById('AiTool-container');
+    tools.forEach(tool => {
+        const toolDiv = document.createElement('div');
+        toolDiv.classList.add('col');
+        toolDiv.innerHTML =`
+        <div class="card h-100">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>`;
+        AiToolContainer.appendChild(toolDiv);
+    });
 }
 
 loadAiTools();
