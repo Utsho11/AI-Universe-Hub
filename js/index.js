db024bbc9fd7a6b657265c1e7ed55e016b762fd1
Utsho11 <utshoroy521@gmail.com>
2023-03-03 17:27:36 +0600

    spinner added

--- js/index.js
+++ js/index.js
@@ -5,8 +5,16 @@ const loadAiTools = async() =>{
     displayAiTools(data.data.tools);
 }
 
-const displayAiTools = tools =>{
+const displayAiTools = (tools) =>{
     const AiToolContainer = document.getElementById('AiTool-container');
+    
+    if(tools.length > 6 ){
+        tools = tools.slice(0,6);
+        document.getElementById('show-more').classList.remove('d-none')
+    }
+    else{
+        document.getElementById('show-more').classList.add('d-none')  
+    }
     tools.forEach(tool => {
         const toolDiv = document.createElement('div');
         toolDiv.classList.add('col');
@@ -26,14 +34,31 @@ const displayAiTools = tools =>{
                           
                           <h5 class="card-title">${tool.name}</h5>
                           <div class="d-flex">
-                          <i class="flex-grow-1 fa-solid fa-calendar-days"><span style="font-size: smaller;"> ${tool.published_in}</span></i>
+                          <i class="flex-grow-1 fa-solid fa-calendar-days" style="font-size: smaller;"><span> ${tool.published_in}</span></i>
                           <button class="border-danger rounded-circle bg-danger" style="--bs-bg-opacity: .4"><i class="fa-solid  fa-arrow-right " style="color: crimson;"></i></button>
                           </div>
                           
                         </div>
                       </div>`;
         AiToolContainer.appendChild(toolDiv);
-    });
+          });
+    toggleSpinner(false);
 }
+
+document.getElementById('btn-show-more').addEventListener('click',function(){
+    toggleSpinner(true);
+    loadAiTools();
+})
+
+const toggleSpinner = isLoading =>{
+    const spinSection = document.getElementById('spinner');
+    if(isLoading){
+        spinSection.classList.remove('d-none');
+    }
+    else{
+        spinSection.classList.add('d-none');
+    }
+}
+
 
 loadAiTools();
