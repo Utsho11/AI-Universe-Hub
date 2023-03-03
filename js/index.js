8efd3010f57fc2ef7fe109d2b6b51233844847ff
Utsho11 <utshoroy521@gmail.com>
2023-03-03 11:35:25 +0600

    card added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('connected');
+const loadAiTools = async() =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAiTools(data.data.tools[0]);
+}
+
+const displayAiTools = tools =>{
+    console.log(tools);
+}
+
+loadAiTools();
